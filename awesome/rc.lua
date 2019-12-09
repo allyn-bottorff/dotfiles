70cea4286786d2e71bcb3bf9f4e1eba6751eaf02
Allyn L. Bottorff <allyn.bottorff@veteransunited.com>
2019-12-09 14:11:05 -0600

    addtional mpd cleanup

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -257,7 +257,7 @@ awful.screen.connect_for_each_screen(function(s)
         s.mytasklist, -- Middle widget
         { -- Right widgets
             layout = wibox.layout.fixed.horizontal,
-            mpd_widget,
+            -- mpd_widget,
             wibox.widget.systray(),
             mytextclock,
             s.mylayoutbox,
