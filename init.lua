1778d2d70abb73f6ddd6f0a11c828024e1525a4d
Allyn L. Bottorff <allyn@b6f.net>
2023-09-16 08:05:10 -0500

    committing location changes before merge

--- init.lua
+++ init.lua
@@ -96,6 +96,27 @@ require('lazy').setup({
   -- Comment 
   'numToStr/Comment.nvim', -- "gc" to comment visual regions/lines
 
+  {
+    "nvim-neorg/neorg",
+    build = ":Neorg sync-parsers",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("neorg").setup {
+        load = {
+          ["core.defaults"] = {}, -- Loads default behaviour
+          ["core.concealer"] = {}, -- Adds pretty icons to your documents
+          ["core.dirman"] = { -- Manages Neorg workspaces
+            config = {
+              workspaces = {
+                notes = "~/Documents/norg",
+              },
+            },
+          },
+        },
+      }
+    end,
+  },
+
   -- FZF
   {
     'junegunn/fzf',
