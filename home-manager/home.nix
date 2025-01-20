9ba83e6d252fdcc8f64f26c8a148332ee9aa8aac
Allyn L. Bottorff <allyn@b6f.net>
2025-01-20 11:55:34 -0600

    added ghostty

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -25,7 +25,7 @@ in
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
-  home.stateVersion = "24.05"; # Please read the comment before changing.
+  home.stateVersion = "24.11"; # Please read the comment before changing.
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
@@ -93,6 +93,7 @@ in
     pkgs.yazi
     pkgs.azure-cli
     pkgsUnstable.hugo
+    pkgs.ghostty
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
